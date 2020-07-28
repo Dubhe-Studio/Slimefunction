64958eea8d3e405bfe1c14d178a2c17434a877df
Gu_ZT <administration@ztxy666.cn>
2020-07-28 14:06:07 +0800

    Update change.py

--- Slimefunction_datapacks/data/sf/loot_tables/weapons/change.py
+++ Slimefunction_datapacks/data/sf/loot_tables/weapons/change.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
 
-text = "{\n\t\"pools\": [\n\t\t{\n\t\t\t\"rolls\": 1,\n\t\t\t\"entries\": [\n\t\t\t\t{\n\t\t\t\t\t\"type\": \"minecraft:item\",\n\t\t\t\t\t\"name\": \"minecraft:firework_star\",\n\t\t\t\t\t\"functions\": [\n\t\t\t\t\t\t{\n\t\t\t\t\t\t\t\"function\": \"minecraft:set_nbt\",\n\t\t\t\t\t\t\t\"tag\": \"{CustomModelData:<>,display:{Name:'{\\\"italic\\\":false,\\\"translate\\\":\\\"<>\\\"}'},id:'<>'}\"\n\t\t\t\t\t\t}\n\t\t\t\t\t]\n\t\t\t\t}\n\t\t\t]\n\t\t}\n\t]\n}"
+text = "{\n\t\"pools\": [\n\t\t{\n\t\t\t\"rolls\": 1,\n\t\t\t\"entries\": [\n\t\t\t\t{\n\t\t\t\t\t\"type\": \"minecraft:item\",\n\t\t\t\t\t\"name\": \"minecraft:<>\",\n\t\t\t\t\t\"functions\": [\n\t\t\t\t\t\t{\n\t\t\t\t\t\t\t\"function\": \"minecraft:set_nbt\",\n\t\t\t\t\t\t\t\"tag\": \"{CustomModelData:727<>,display:{Name:'{\\\"translate\\\":\\\"<>\\\"}'},id:'sf:<>'}\"\n\t\t\t\t\t\t}\n\t\t\t\t\t]\n\t\t\t\t}\n\t\t\t]\n\t\t}\n\t]\n}"
 
 dirs = os.listdir(Path())
 for i in dirs:
