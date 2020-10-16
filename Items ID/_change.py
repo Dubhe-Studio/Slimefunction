60137c16e21b2fe44773949e2873255211bb19a3
Cjsah <2684117397@qq.com>
2020-10-16 21:47:35 +0800

    Update _change.py

--- Items ID/_change.py
+++ Items ID/_change.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
 
-text = "{{\n\t\"pools\": [\n\t\t{{\n\t\t\t\"rolls\": 1,\n\t\t\t\"entries\": [\n\t\t\t\t{{\n\t\t\t\t\t\"type\": \"minecraft:item\",\n\t\t\t\t\t\"name\": \"minecraft:<>\",\n\t\t\t\t\t\"functions\": [\n\t\t\t\t\t\t{{\n\t\t\t\t\t\t\t\"function\": \"minecraft:set_nbt\",\n\t\t\t\t\t\t\t\"tag\": \"{{CustomModelData:7270<>,display:{{Lore:['{{\\\"italic\\\":false,\\\"translate\\\":\\\"sf.{name}.lore\\\"}}'],Name:'{{\\\"italic\\\":false,\\\"translate\\\":\\\"sf.{name}.name\\\"}}'}},id:'sf:{name}'}}\"\n\t\t\t\t\t\t}}\n\t\t\t\t\t]\n\t\t\t\t}}\n\t\t\t]\n\t\t}}\n\t]\n}}"
+text = "{{\n\t\"pools\": [\n\t\t{{\n\t\t\t\"rolls\": 1,\n\t\t\t\"entries\": [\n\t\t\t\t{{\n\t\t\t\t\t\"type\": \"minecraft:item\",\n\t\t\t\t\t\"name\": \"minecraft:\",\n\t\t\t\t\t\"functions\": [\n\t\t\t\t\t\t{{\n\t\t\t\t\t\t\t\"function\": \"minecraft:set_nbt\",\n\t\t\t\t\t\t\t\"tag\": \"{{CustomModelData:7270,display:{{Lore:['{{\\\"italic\\\":false,\\\"translate\\\":\\\"sf.{name}.lore\\\"}}'],Name:'{{\\\"italic\\\":false,\\\"translate\\\":\\\"sf.{name}.name\\\",\\\"color\\\":\\\"\\\"}}'}},id:'sf:{name}',Enchantments:[{{id:\\\"\\\",lvl:}}]}}\"\n\t\t\t\t\t\t}}\n\t\t\t\t\t]\n\t\t\t\t}}\n\t\t\t]\n\t\t}}\n\t]\n}}"
 
 dirs = os.listdir(Path())
 for i in dirs:
